e9e66ecaeb317f861882d9867d74fb3942df591a
Louis Byers <louis@codebyers.com>
2024-03-02 11:33:14 -0800

    Added a shared types lib and moved the declared interfaces to there.

--- apps/student-app/src/app/student-app.tsx
+++ apps/student-app/src/app/student-app.tsx
@@ -1,28 +1,6 @@
-import React, {Component} from 'react'
+import React from 'react';
 import { useState } from 'react';
-interface Author {
-  id: string,
-  email: string,
-  first_name: string,
-  last_name: string
-}
-interface Word {
-  word: string,
-  hint: string
-}
-interface WordList {
-  id: string,
-  name: string,
-  words: any
-}
-interface Data {
-  author: Author,
-  id: string,
-  course_title: string,
-  course_code: string,
-  last_updated: Date,
-  word_lists: any
-}
+import { Data, WordList, Word } from '@vocabulary-game/types';
 
 function get_test_data(course_code: string) {
   return test_data();
@@ -30,32 +8,32 @@ function get_test_data(course_code: string) {
 function test_data(): Data {
   return {
     author: {
-      id: "42",
-      email: "useremail@gmail.com",
-      first_name: "Educator",
-      last_name: "User"
+      id: '42',
+      email: 'useremail@gmail.com',
+      first_name: 'Educator',
+      last_name: 'User',
     },
-    id: "1",
-    course_title: "WR 95",
-    course_code: "QWERTY",
+    id: '1',
+    course_title: 'WR 95',
+    course_code: 'QWERTY',
     last_updated: new Date(),
     word_lists: [
       {
-        id: "1",
-        name: "WR 95 Test List",
-        word_list: [
+        id: '1',
+        name: 'WR 95 Test List',
+        words: [
           {
-            word: "buttress", 
-            hint: "A structure built against a wall in order to support or strengthen it."
+            word: 'buttress',
+            hint: 'A structure built against a wall in order to support or strengthen it.',
           },
           {
-            word: "prestigious",
-            hint: "Something having the respect and admiration that someone or something gets for being successful or important."
-          }
-        ]
-      }
-    ]
-  }
+            word: 'prestigious',
+            hint: 'Something having the respect and admiration that someone or something gets for being successful or important.',
+          },
+        ],
+      },
+    ],
+  };
 }
 
 export function StudentApp({ title }: { title: string }) {
@@ -72,21 +50,22 @@ export function StudentApp({ title }: { title: string }) {
   var time_remaining = 60;
   var word_list: WordList;
   var hint = '';
-  var word = ''
+  var word = '';
   var answered = false;
-  var is_correct = false
+  var is_correct = false;
   var words_in_game: Word[] = [];
   // Score Related Methods
   function increment_score() {
     score++;
   }
   function set_word_list(selected_list: WordList) {
-    word_list = selected_list
+    word_list = selected_list;
   }
 
   // Pick random word from word list
-  function next_word() { 
-    const word_obj = word_list.words[Math.floor(Math.random() * word_list.words.length)]; 
+  function next_word() {
+    const word_obj =
+      word_list.words[Math.floor(Math.random() * word_list.words.length)];
     hint = word_obj.hint;
     word = word_obj.word;
     words_in_game.push(word_obj);
@@ -104,118 +83,131 @@ export function StudentApp({ title }: { title: string }) {
   // Idea is to set the word list here, then confirm later in the form
   function get_word_list_items() {
     const listItems = data.word_lists.map((word_list: WordList) => {
-      return <li onClick={((e) => handleSetWordList(e, word_list.id))} id={word_list.id}><b>{word_list.name}:</b> word_list.words.length</li>
+      return (
+        <li
+          onClick={(e) => handleSetWordList(e, word_list.id)}
+          id={word_list.id}
+        >
+          <b>{word_list.name}:</b> word_list.words.length
+        </li>
+      );
     });
-    wordLists = {listItems};
+    wordLists = { listItems };
   }
 
-  // Creates a list of words that were in the game. 
+  // Creates a list of words that were in the game.
   // Populates a UL element on game-words page.
   function get_words_from_game() {
     const listItems = words_in_game.map((word: Word) => {
-      return <li  id='word${word.word}'><b>word.word:</b> word.hint</li>
+      return (
+        <li id="word${word.word}">
+          <b>word.word:</b> word.hint
+        </li>
+      );
     });
-    return {listItems};
+    return { listItems };
   }
 
   // Button to go to Crouse page
   function handleToCourse(e: any) {
     try {
-      setError('null')
-      setScreen('course')
+      setError('null');
+      setScreen('course');
     } catch (err) {
-      setError('error-word-list')
+      setError('error-word-list');
     }
   }
 
   // Back button to return to Splash page
   function handleBackToSplash(e: any) {
     try {
-      setError('null')
-      setScreen('splash')
+      setError('null');
+      setScreen('splash');
     } catch (err) {
-      setError('error-course')
+      setError('error-course');
     }
   }
 
   // Back button to return to Course Code page
   function handleBackToCourse(e: any) {
     try {
-      setError('null')
-      setScreen('course')
+      setError('null');
+      setScreen('course');
     } catch (err) {
-      setError('error-word-list')
+      setError('error-word-list');
     }
   }
 
-   // Back button to return to Word List page
-   function handleBackToWordLists(e: any) {
+  // Back button to return to Word List page
+  function handleBackToWordLists(e: any) {
     try {
-      setError('null')
-      setScreen('word-list')
+      setError('null');
+      setScreen('word-list');
     } catch (err) {
-      setError('error-word-list')
+      setError('error-word-list');
     }
   }
 
   // Back button to return to Score page
   function handleBackToScore(e: any) {
-   try {
-     setError('null')
-     setScreen('score')
-   } catch (err) {
-   }
- }
+    try {
+      setError('null');
+      setScreen('score');
+    } catch (err) {}
+  }
 
   // Replay button that resets the game and starts it again.
   function handleResetGame(e: any) {
     try {
-      setError('null')
-      reset_game()
-      setScreen('game')
-    } catch (err) {
-    }
+      setError('null');
+      reset_game();
+      setScreen('game');
+    } catch (err) {}
   }
 
- // Button to visit Word Review page
- function handleToGameWords(e: any) {
-  try {
-    setError('null')
-    setScreen('game-words')
-  } catch (err) {
+  // Button to visit Word Review page
+  function handleToGameWords(e: any) {
+    try {
+      setError('null');
+      setScreen('game-words');
+    } catch (err) {}
   }
-}
 
   // Handles course code submission
   async function handleSubmitCourse(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
       // GET on course GUID
-      await submitCourse(course_code)
-      
+      await submitCourse(course_code);
+
       data = get_test_data(course_code);
-      get_word_list_items()
-      setError('null')
-      setScreen('word-list')
+      get_word_list_items();
+      setError('null');
+      setScreen('word-list');
     } catch (err) {
-      setError('error-course')
+      setError('error-course');
     }
   }
 
   // Handles setting the word list to chosen list item ID
-  async function handleSetWordList(e: React.MouseEvent<HTMLLIElement, MouseEvent>, id: string) {
+  async function handleSetWordList(
+    e: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    id: string
+  ) {
     try {
       word_list = data.word_lists.filter((wrd_lst: WordList) => {
         return wrd_lst.id === id;
       });
       setError('null');
     } catch (err) {
-      setError('error-word-list')
+      setError('error-word-list');
     }
   }
 
   // Handles text entry into Course Code Text field
-  function handleCourseChange(e: { target: { value: React.SetStateAction<string>; }; }) {
+  function handleCourseChange(e: {
+    target: { value: React.SetStateAction<string> };
+  }) {
     setCourse(e.target.value);
   }
 
@@ -227,121 +219,172 @@ export function StudentApp({ title }: { title: string }) {
       word_list = {
         id: '',
         name: '',
-        words: []
+        words: [],
       };
-    } catch (err) {
-
-    }
+    } catch (err) {}
   }
 
   // Handles word submission. I'll probably change this to a correct / skip button later to simulate head nods and shakes
   async function handleSubmitWord(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
-      answered = true
+      answered = true;
       // await submitWord(e.target.value);
       // get answer
       //check answer against word, if lower cases match, is_correct = true
-      is_correct = true
+      is_correct = true;
     } catch (err) {
-      is_correct = false
+      is_correct = false;
     }
   }
 
   return (
     <>
-      <div id="splash-screen" className={screen === 'splash' ? "content" : "hidden-content"}>
+      <div
+        id="splash-screen"
+        className={screen === 'splash' ? 'content' : 'hidden-content'}
+      >
         <h1>Vocab Game</h1>
         <div>Splash Screen</div>
         <div>tap screen to start</div>
-        <button onClick={handleToCourse} id="start-btn" disabled={
-                false
-              }>  
-            Start
-          </button>
+        <button onClick={handleToCourse} id="start-btn" disabled={false}>
+          Start
+        </button>
       </div>
 
-
-      <div id="course-selection" className={screen === 'course' ? 'content' : 'hidden-content'}>
+      <div
+        id="course-selection"
+        className={screen === 'course' ? 'content' : 'hidden-content'}
+      >
         <h1>Vocab Game</h1>
         <div id="course-select">
-          <form id="course-select-form"  onSubmit={handleSubmitCourse}>
-            {error === 'error-course' ? <span id='errorCourse' className='error'>Failed to load course guid.</span> : null}
+          <form id="course-select-form" onSubmit={handleSubmitCourse}>
+            {error === 'error-course' ? (
+              <span id="errorCourse" className="error">
+                Failed to load course guid.
+              </span>
+            ) : null}
             <label>Enter Course Code: </label>
-            <input type="text" id="course-guid" name="course-guid" placeholder="qwerty" value={course_code} onChange={handleCourseChange}/><br></br>
-            <button type="submit" id="submit-course-code"  
-              
-              disabled={
-                false
-              }>
+            <input
+              type="text"
+              id="course-guid"
+              name="course-guid"
+              placeholder="qwerty"
+              value={course_code}
+              onChange={handleCourseChange}
+            />
+            <br></br>
+            <button type="submit" id="submit-course-code" disabled={false}>
               Submit
             </button>
-            
           </form>
           <button id="back-to-splash-btn" onClick={handleBackToSplash}>
-              Back
-            </button>
-        </div>
-      </div>
-      
-      
-      <div id="word-list-selection" className={screen === 'word-list' ? "content"  : "hidden-content"}>
-        <h1>Vocab Game</h1>
-        {error === 'error-course' ? <span id='errorCourse' className='error'>Failed to load course guid.</span> : null}
-        <div id="word-list-select">
-          <form id="course-select-form"  onSubmit={handleSubmitCourse}>
-            {error === 'error-word-list' ? <span id='errorCourse' className='error'>Failed to load course guid.</span> : null}
-            <label>Select Word List: </label>
-            <ul id="word-list-list">
-              {wordLists}
-            </ul>
-            <button type="submit" id="submit-word-list-btn" >
-              Submit
-            </button>
-          </form>
-          <button  id="back-to-course-btn" onClick={handleBackToCourse}>
-              Back
+            Back
           </button>
         </div>
       </div>
 
-      
-      <div id="game-screen" className={screen === 'game' ? "content"  : "hidden-content"} >
-      <form id="course-select-form" onSubmit={handleSubmitWord}>
-      <h1 id="word-hint-header">{hint}</h1>
-        <label>Enter Word: </label>
-        <input type="text" id="word-test" name="word-test" placeholder="word"/><br></br>
-        <button disabled={
-            false
-          }>
-          Check
+      <div
+        id="word-list-selection"
+        className={screen === 'word-list' ? 'content' : 'hidden-content'}
+      >
+        <h1>Vocab Game</h1>
+        {error === 'error-course' ? (
+          <span id="errorCourse" className="error">
+            Failed to load course guid.
+          </span>
+        ) : null}
+        <div id="word-list-select">
+          <form id="course-select-form" onSubmit={handleSubmitCourse}>
+            {error === 'error-word-list' ? (
+              <span id="errorCourse" className="error">
+                Failed to load course guid.
+              </span>
+            ) : null}
+            <label>Select Word List: </label>
+            <ul id="word-list-list">{wordLists}</ul>
+            <button type="submit" id="submit-word-list-btn">
+              Submit
+            </button>
+          </form>
+          <button id="back-to-course-btn" onClick={handleBackToCourse}>
+            Back
+          </button>
+        </div>
+      </div>
+
+      <div
+        id="game-screen"
+        className={screen === 'game' ? 'content' : 'hidden-content'}
+      >
+        <form id="course-select-form" onSubmit={handleSubmitWord}>
+          <h1 id="word-hint-header">{hint}</h1>
+          <label>Enter Word: </label>
+          <input
+            type="text"
+            id="word-test"
+            name="word-test"
+            placeholder="word"
+          />
+          <br></br>
+          <button disabled={false}>Check</button>
+          <div id="word-answer">
+            <h2
+              id="word-answer-header"
+              className={
+                answered
+                  ? is_correct
+                    ? 'correct-word'
+                    : 'incorrect-word'
+                  : 'word'
+              }
+            >
+              {word}
+            </h2>
+          </div>
+          <div id="score">Score: {score}</div>
+          <div id="time-left">
+            {time_remaining} {time_remaining === 1 ? 'second' : 'seconds'}
+          </div>
+        </form>
+      </div>
+
+      <div
+        id="game-screen"
+        className={screen === 'score' ? 'content' : 'hidden-content'}
+      >
+        <h1 id="score-header">Congrats!</h1>
+        <div>
+          You got {score} of {words_in_game.length}!
+        </div>
+        <button id="back-to-course-btn" onClick={handleResetGame}>
+          Replay
         </button>
-        <div id="word-answer"><h2 id="word-answer-header" className={answered ?  is_correct? "correct-word": "incorrect-word" : "word"}>{word}</h2></div>
-        <div id="score">Score: {score}</div>
-        <div id="time-left">{time_remaining} {time_remaining === 1 ? 'second' : 'seconds'}</div>
-      </form>
+        <button id="game-words-btn" onClick={handleToGameWords}>
+          Review Words
+        </button>
+        <br></br>
+
+        <button id="back-to-word-list-btn" onClick={handleBackToWordLists}>
+          New Wordlist
+        </button>
+        <button id="back-to-course-btn" onClick={handleBackToCourse}>
+          Change Course
+        </button>
       </div>
 
-      
-      <div id="game-screen" className={screen === 'score' ? "content"  : "hidden-content"} >
-      <h1 id="score-header">Congrats!</h1>
-      <div>You got {score} of {words_in_game.length}!</div>
-          <button id="back-to-course-btn" onClick={handleResetGame}>Replay</button>
-          <button id="game-words-btn" onClick={handleToGameWords}>Review Words</button><br></br>
-
-          <button id="back-to-word-list-btn" onClick={handleBackToWordLists}>New Wordlist</button>
-          <button id="back-to-course-btn" onClick={handleBackToCourse}>Change Course</button>
-      </div>
-
-      
-      <div id="game-screen" className={screen === 'game-words' ? "content"  : "hidden-content"} >
+      <div
+        id="game-screen"
+        className={screen === 'game-words' ? 'content' : 'hidden-content'}
+      >
         <h1>Vocab Game</h1>
         <ul>
-          <li><b>Word</b>: <i>hint</i></li>
+          <li>
+            <b>Word</b>: <i>hint</i>
+          </li>
           {}
         </ul>
-        <button onClick={handleBackToScore} >Back to Score</button>
-
+        <button onClick={handleBackToScore}>Back to Score</button>
       </div>
     </>
   );
@@ -352,7 +395,7 @@ function submitCourse(code: string) {
   // Pretend it's hitting the network.
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      let shouldError = code.toLowerCase() !== 'qwerty'
+      let shouldError = code.toLowerCase() !== 'qwerty';
       if (shouldError) {
         reject(new Error('Unable to find course: "' + code + '".'));
       } else {
